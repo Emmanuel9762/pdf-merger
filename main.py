a4d09051ad5d594270a465282dde76dc1772bc71
Emmanuel <nlihleemmanuel@gmail.com>
2026-08-29 20:39:22 +0200

    normalised formats

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ output_folder = Path("output")
 
 pdf_files = list(input_folder.glob("*.pdf"))
 
-print(pdf_files)
+pdf_files = [
+    file for file in input_folder.iterdir()
+    if file.is_file() and file.suffix.lower() == ".pdf"
+]
+
+print("pdf_files:", pdf_files)
