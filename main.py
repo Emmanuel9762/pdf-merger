61bdf36858898668e865d6cea9fc3f2b1b7e54a2
Emmanuel <nlihleemmanuel@gmail.com>
2026-08-31 21:50:58 +0200

    feat: allow users to choose merge order

--- main.py
+++ main.py
@@ -6,21 +6,36 @@ from pypdf import PdfWriter
 input_folder = Path("input")
 output_folder = Path("output")
 
-pdf_files = list(input_folder.glob("*.pdf"))
-
+output_folder.mkdir(exist_ok=True)
 
 # Filter for PDF files specifically
 pdf_files = [
     file for file in input_folder.iterdir()
     if file.is_file() and file.suffix.lower() == ".pdf"
 ]
+if not pdf_files:
+    print("No PDF files found.")
+    exit()
 
-print("PDFs found:")
+print("\nPDFs found:")
+
+for index, pdf_file in enumerate(pdf_files, start=1):
+    print(f"{index}. {pdf_file.name}")
+
+order = input("\nEnter the order you want (e.g 1 2 3): ")
+
+order = [int(number) for number in order.split()]
+
+selected_files = [pdf_files[index - 1] for index in order]
+
+print("\nMerge Order:")
+
+for index, pdf_file in enumerate (selected_files, start=1):
+    print(f"{index}. {pdf_file.name}")
 
 writer = PdfWriter()
 
-# Combine all PDFs 
-for pdf_file in pdf_files: 
+for pdf_file in selected_files: 
     writer.append(pdf_file)
 
 output_file = output_folder / "merged.pdf"
